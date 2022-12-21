4f522134dc65d91cc47587c6d8eae8179739a331
Dani <semdaniel1@gmail.com>
2022-12-21 21:27:59 +0200

    added navigation dependency and started entry screen

--- app/src/main/java/com/example/pal/ui/MainActivity.kt
+++ app/src/main/java/com/example/pal/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pal
+package com.example.pal.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pal.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/pal/ui/entry/EntryFragment.kt
@@ -0,0 +1,29 @@
+package com.example.pal.ui.entry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pal.databinding.FragmentEntryBinding
+import il.co.syntax.fullarchitectureretrofithiltkotlin.utils.autoCleared
+
+class EntryFragment : Fragment() {
+
+    private var binding : FragmentEntryBinding by autoCleared()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentEntryBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
